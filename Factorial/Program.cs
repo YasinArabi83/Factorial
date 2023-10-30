d44f97860898ac51994f75dec3b025fe6d4a03b5
Acer <Acer@DESKTOP-JS0O9TD>
2023-10-30 20:34:55 +0330

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿//یاسین عربی
+//فاکتوریال عدد n 
+int n =0;
+Console.Write("enter a number :");
+n = int.Parse(Console.ReadLine());
+    long result = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        result = result * i;
+    }
+Console.Write("Factorial: ");
+Console.WriteLine( result);
